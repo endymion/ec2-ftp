1536d01b56e078e1b4aa3a60d5f2db24db5c8b3f
Ryan Alyn Porter <rap@endymion.com>
2014-08-22 15:44:28 -0400

    Enable SFTP, but don't require it.
    Un-comment the commented lines at the end of vsftpd.conf to require SFTP.

--- cookbooks/development/configure/recipes/default.rb
+++ cookbooks/development/configure/recipes/default.rb
@@ -4,6 +4,25 @@ cookbook_file "vsftpd.conf" do
   path "/etc/vsftpd.conf"
 end
 
+execute "create SSL certificate for SFTP" do
+  user "root"
+  command "openssl req -x509 -nodes -days 3650 -newkey rsa:1024 -subj '/C=US/ST=Nevada/L=Las Vegas/O=HakkasanGroup/CN=ftp.vault.venuedriver.com' -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem"
+  not_if { ::File.exists?('/etc/ssl/private/vsftpd.pem')}
+end
+
+execute "Configure sshd - allow password" do
+  user "root"
+  command <<-EOF
+  sed -i 's/^.*PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+  EOF
+  only_if "egrep 'PasswordAuthentication no' /etc/ssh/sshd_config"
+end
+
+execute "restart sshd" do
+  user "root"
+  command "service ssh restart"
+end
+
 execute "restart vsftpd" do
   user "root"
   command "service vsftpd restart"
@@ -20,7 +39,7 @@ ftp_users = YAML.load_file('/vagrant/users.yml').each do |ftp_user|
   end
   directory "/home/#{username}" do
     owner username
-    mode 0444
+    mode 0500
     action :create
   end
 end
